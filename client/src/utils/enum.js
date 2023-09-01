51e00c4f4086b7c846e51ba52b4aed804829ed2d
Jose Lopez <jose.lopezgarrido95@gmail.com>
2023-09-01 13:44:48 -0400

    a

--- client/src/utils/enum.js
+++ client/src/utils/enum.js
@@ -214,8 +214,7 @@ module.exports.dialogueObject = {
 };
 
 // Engines
-// const engine = "https://katespracticespace.com";
-const engine = "http://joses-macbook-pro-3.local:8080";
+const engine = "https://katespracticespace.com";
 module.exports.URLS = {
   // Authentication endpoins
   registerUri: `${engine}/api/v1/auth/register`,
